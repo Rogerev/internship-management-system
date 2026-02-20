d77070e510ee42d24cd9509e1d2e03f1ff9e4af6
Rogerev <patoletanmay05@gmail.com>
2026-02-20 15:10:26 +0530

    Change database password to a placeholder
    Updated the database password to a placeholder.

--- src/com/internship/db/DBConnection.java
+++ src/com/internship/db/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
     private static final String URL =
             "jdbc:mysql://localhost:3306/internship_db?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "tanmay-riya";
+    private static final String PASSWORD = "your_database_password";
 
     public static Connection getConnection() {
         try {
